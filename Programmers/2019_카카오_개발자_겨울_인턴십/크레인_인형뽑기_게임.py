5f6d14c553896ea266ef94ac803848f243e0c2d3
dgh03207 <58917737+dgh03207@users.noreply.github.com>
2021-04-22 19:14:04 +0900

    commit on 'main

--- Programmers/2019_카카오_개발자_겨울_인턴십/크레인_인형뽑기_게임.py
+++ Programmers/2019_카카오_개발자_겨울_인턴십/크레인_인형뽑기_게임.py
@@ -5,21 +5,21 @@ def solution(board, moves):
     result = []
     cnt = 0
     for m in moves:
-        for n in range(len(board)):
-            if board[n][m-1] != 0:
-                if len(result) > 0:
-                    if board[n][m-1] == result[-1]:
+        for brd in board:
+            if brd[m-1] != 0:
+                if len(result)>0:
+                    if brd[m-1] == result[-1]:
                         result.pop()
-                        cnt += 2
-                        board[n][m-1] = 0
+                        cnt+=2
+                        brd[m-1] = 0
                         break
                     else:
-                        result.append(board[n][m-1])
-                        board[n][m-1] = 0
+                        result.append(brd[m-1])
+                        brd[m-1] = 0
                         break
                 else:
-                    result.append(board[n][m-1])
-                    board[n][m-1] = 0
+                    result.append(brd[m-1])
+                    brd[m-1] = 0
                     break
                 
     return cnt
--- Programmers/월간_코드_챌린지_시즌1/3진법_뒤집기.py
+++ Programmers/월간_코드_챌린지_시즌1/3진법_뒤집기.py
@@ -1,3 +1,11 @@
 #programmers
 #월간 코드 챌린지 시즌1_3진법 뒤집기
 
+def solution(n):
+    result = ""
+    while(n>0):
+        print(n)
+        n,r = divmod(n,3)
+        result += str(r)
+        
+    return int(result,3)
