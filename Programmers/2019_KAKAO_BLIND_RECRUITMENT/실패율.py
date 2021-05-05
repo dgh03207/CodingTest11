07fb3cf577f9a8025f1beb0b8b4bcb84da655578
dgh03207 <58917737+dgh03207@users.noreply.github.com>
2021-05-05 16:41:00 +0900

    commit on 'main'

--- Programmers/2019_KAKAO_BLIND_RECRUITMENT/실패율.py
+++ Programmers/2019_KAKAO_BLIND_RECRUITMENT/실패율.py
@@ -1,3 +1,23 @@
 #programmers
 #2019 KAKAO BLIND RECRUITMENT_실패율
 
+from collections import Counter
+
+
+def solution(N, stages):
+    n =1
+    length = len(stages)
+    stages = Counter(sorted(stages))
+    result = []
+    
+    while(n<N+1):
+        if length == 0:
+            result.append([n,0])
+        else:
+            result.append([n,stages[n]/length])
+            length -= stages[n]
+        n += 1
+        
+    result = sorted(result,key = lambda x: x[1],reverse = True)
+    
+    return [x[0] for x in result]
