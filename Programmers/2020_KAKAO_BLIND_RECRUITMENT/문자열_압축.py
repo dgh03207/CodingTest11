c9b178b71c7bc2b80ba0e155f977cf5885d0fb39
dgh03207 <58917737+dgh03207@users.noreply.github.com>
2021-05-17 23:41:06 +0900

    commit on 'codingtest'

--- Programmers/2020_KAKAO_BLIND_RECRUITMENT/문자열_압축.py
+++ Programmers/2020_KAKAO_BLIND_RECRUITMENT/문자열_압축.py
@@ -1,3 +1,34 @@
 #programmers
 #2020 KAKAO BLIND RECRUITMENT_문자열 압축
 
+def solution(s):
+    
+    answer = s
+    
+    for i in range(1,len(s)):
+        prev = s[0:0+i]
+        
+        a=0
+        cnt = 1
+        temp_answer = ""
+        
+        while a<len(s):
+            a=a+i
+                
+            if prev == s[a:a+i]:
+                cnt +=1
+            else:
+                if cnt == 1:
+                    temp_answer += prev
+                else:
+                    temp_answer += prev+str(cnt)
+                
+                prev=s[a:a+i]
+                cnt = 1
+                
+
+    
+        if len(temp_answer) < len(answer):
+            answer = temp_answer
+
+    return len(answer)
