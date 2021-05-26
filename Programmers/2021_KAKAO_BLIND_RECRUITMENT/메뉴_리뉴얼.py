ee8eefddbf6f159d0c19243fc5aa70c8b5ee1c76
dgh03207 <58917737+dgh03207@users.noreply.github.com>
2021-05-27 00:23:18 +0900

    commit on 'codingtest'

--- Programmers/2021_KAKAO_BLIND_RECRUITMENT/메뉴_리뉴얼.py
+++ Programmers/2021_KAKAO_BLIND_RECRUITMENT/메뉴_리뉴얼.py
@@ -1,2 +1,20 @@
 #programmers
 #2021 KAKAO BLIND RECRUITMENT_메뉴 리뉴얼
+
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    
+    answer = []
+
+    for c in course:
+        temp = []
+        for order in orders:
+            temp += combinations(sorted(order),c)
+            
+        count = Counter(temp)
+        if len(count) != 0 and max(count.values()) != 1:
+            answer += [''.join(val) for val in count if count[val] == max(count.values())]
+     
+    return sorted(answer)
