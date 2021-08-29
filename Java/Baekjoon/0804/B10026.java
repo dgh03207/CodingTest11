fe88d78c8ef3dad3cef635e94de5d7d251314d22
AYEONG <dgh03207@gmail.com>
2021-08-30 02:25:43 +0900

    Revise : Encoding problem

--- Java/Baekjoon/0804/B10026.java
+++ Java/Baekjoon/0804/B10026.java
@@ -5,12 +5,12 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-//�쟻濡앹깋�빟
+//적록색약
 public class B10026 {
 	
 	static int N,answer;
 	static String[][] paint,paint_RG;
-	static int[] dr = {-1,1,0,0};	//�긽�븯醫뚯슦 �깘�깋
+	static int[] dr = {-1,1,0,0};	//상하좌우 탐색
 	static int[] dc = {0,0,-1,1};
 	static boolean[][] isChecked;
 	static StringBuilder sb = new StringBuilder();
@@ -21,36 +21,36 @@ public class B10026 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
-		paint = new String[N][N];			//洹몃┝ RGB�젙蹂대�� �떞�뒗 諛곗뿴
-		paint_RG = new String[N][N];		//RG瑜� �븯�굹濡� 留뚮뱺 �젙蹂대�� �떞�뒗 諛곗뿴
-		isChecked = new boolean[N][N];		//�씠誘� 吏��굹�삩 寃쎈줈瑜� 泥댄겕�븯�뒗 諛곗뿴
+		paint = new String[N][N];			//그림 RGB정보를 담는 배열
+		paint_RG = new String[N][N];		//RG를 하나로 만든 정보를 담는 배열
+		isChecked = new boolean[N][N];		//이미 지나온 경로를 체크하는 배열
 		
-		for (int i = 0; i < N; i++) {		//for臾몄쑝濡� 洹몃┝ �젙蹂대�� paint�뿉 �떞�뒗�떎. 
+		for (int i = 0; i < N; i++) {		//for문으로 그림 정보를 paint에 담는다. 
 			String line = br.readLine();
-			paint[i] = line.split("");		//�씪諛� 洹몃┝ �젙蹂대�� �떞�뒗�떎.
-			line = line.replace("R","G");	//R�쓣 G濡� 蹂��솕�븳�떎.
-			paint_RG[i] = line.split("");	//R�쓣 G濡� 留뚮뱺 洹몃┝ �젙蹂대�� 諛쏅뒗�떎.
+			paint[i] = line.split("");		//일반 그림 정보를 담는다.
+			line = line.replace("R","G");	//R을 G로 변화한다.
+			paint_RG[i] = line.split("");	//R을 G로 만든 그림 정보를 받는다.
 		}
 		
 		
  
 		isChecked = new boolean[N][N];
-		queue.add(new int[] {0,0});			//�걧�뿉 (0.0)�쓣 �꽔�뒗�떎.
-		while(!queue.isEmpty()) {			//�걧媛� 鍮꾩뼱�엳吏� �븡�쑝硫� while臾몄쓣 怨꾩냽 �룉�떎.
-			int[] dir = queue.poll();		//�걧�뿉�꽌 �젙蹂대�� 鍮쇰궦�떎.
-			int x = dir[0];					//x瑜� 諛쏆븘�삩�떎. 
-			int y = dir[1];					//y瑜� 諛쏆븘�삩�떎.
+		queue.add(new int[] {0,0});			//큐에 (0.0)을 넣는다.
+		while(!queue.isEmpty()) {			//큐가 비어있지 않으면 while문을 계속 돈다.
+			int[] dir = queue.poll();		//큐에서 정보를 빼낸다.
+			int x = dir[0];					//x를 받아온다. 
+			int y = dir[1];					//y를 받아온다.
 			
-			if(!isChecked[y][x]) {			//isChecked�뿉 泥댄겕媛� �릺�뼱 �엳吏� �븡�쑝硫�
-				search(paint[y][x],x,y);	//search()臾몄쓣 �룎由곕떎. 
-				answer ++;					//answer�쓣 利앷��떆�궓�떎.
+			if(!isChecked[y][x]) {			//isChecked에 체크가 되어 있지 않으면
+				search(paint[y][x],x,y);	//search()문을 돌린다. 
+				answer ++;					//answer을 증가시킨다.
 			}
 			
 		}
 		System.out.print(answer+" ");
 		
-		queue.add(new int[] {0,0});			//�걧�뿉 (0.0)�쓣 �꽔�뒗�떎.
-		paint = paint_RG.clone();			//paint�뿉 �쟻濡앹쓣 異붽��븳�떎. 
+		queue.add(new int[] {0,0});			//큐에 (0.0)을 넣는다.
+		paint = paint_RG.clone();			//paint에 적록을 추가한다. 
 		isChecked = new boolean[N][N];
 		answer = 0;
 		
@@ -78,11 +78,11 @@ public class B10026 {
 		
 		if(!isChecked[y][x]) {
 			isChecked[y][x] = true;
-			for (int n = 0; n < 4; n++) {	//4諛⑺깘�깋
+			for (int n = 0; n < 4; n++) {	//4방탐색
 				int nx = dc[n]+x;
 				int ny = dr[n]+y;
 				
-				if(!isRange(nx,ny) && !isChecked[ny][nx]) {	//�씠誘� �깘�깋�뻽�뜕 援ш컙�씠 �븘�땲�뼱�빞�븿.
+				if(!isRange(nx,ny) && !isChecked[ny][nx]) {	//이미 탐색했던 구간이 아니어야함.
 					if(target.equals(paint[ny][nx])) {
 						search(target,nx,ny);
 					}else {
