bfc974ad07dc0b9994c65e0144809b6412e3147d
AYEONG <dgh03207@gmail.com>
2021-08-29 23:18:47 +0900

    Revise : B2740 and B1840 file

--- Java/Baekjoon/0804/B18429.java
+++ Java/Baekjoon/0804/B18429.java
@@ -2,60 +2,63 @@ package Algo_0804;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class B18429 {
 
-	static int N,K,answer;
+	static int N, K, answer;
 	static int[] tools;
 	static int[] combi;
 	static boolean[] isSelected;
-	public static void main(String[] args) throws Exception{
-		
+
+	public static void main(String[] args) throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer (br.readLine());
-	
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		tools = new int[N];
 		combi = new int[N];
 		answer = 0;
 		isSelected = new boolean[N];
-		
+
 		st = new StringTokenizer(br.readLine());
 		for (int n = 0; n < N; n++) {
 			tools[n] = Integer.parseInt(st.nextToken());
 		}
-		
+
 		Permutation(0);
 		System.out.println(answer);
 	}
+
 	private static void Permutation(int cnt) {
-		if(cnt == N) {
-			if(isAvailable(combi)) {
+		if (cnt == N) {
+			if (isAvailable(combi)) {
 				answer += 1;
 			}
 			return;
 		}
-		
+
 		for (int i = 0; i < N; i++) {
-			
-			if(isSelected[i]) continue;
-			
+
+			if (isSelected[i])
+				continue;
+
 			combi[cnt] = tools[i];
 			isSelected[i] = true;
-			Permutation(cnt+1);
+			Permutation(cnt + 1);
 			isSelected[i] = false;
 		}
 	}
-	
+
 	private static boolean isAvailable(int[] combi) {
 		int weight = 500;
-		
+
 		for (int i : combi) {
-			weight += -K+i;
-			if(weight<500) return false;
+			weight += -K + i;
+			if (weight < 500)
+				return false;
 		}
 		return true;
 	}
--- Java/Baekjoon/0804/B2740.java
+++ Java/Baekjoon/0804/B2740.java
@@ -1,24 +1,23 @@
 package Algo_0804;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B2740 {
-	static int N,M,K;
-	static int[][] a_list,b_list,answer_list;
+	static int N, M, K;
+	static int[][] a_list, b_list, answer_list;
 	static StringBuilder sb = new StringBuilder();
-	
+
 	public static void main(String[] args) throws Exception {
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		a_list = new int[N][M];
-		for (int n = 0; n < N; n++) {
+		a_list = new int[N][M];					
+		for (int n = 0; n < N; n++) {			
 			st = new StringTokenizer(br.readLine());
 			for (int m = 0; m < M; m++) {
 				a_list[n][m] = Integer.parseInt(st.nextToken());
@@ -27,7 +26,7 @@ public class B2740 {
 		st = new StringTokenizer(br.readLine());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		
+
 		b_list = new int[M][K];
 		for (int m = 0; m < M; m++) {
 			st = new StringTokenizer(br.readLine());
@@ -35,27 +34,27 @@ public class B2740 {
 				b_list[m][k] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+
 		answer_list = new int[N][K];
-		
-		multiply(N,M,K);
+
+		multiply(N, M, K);
 		System.out.println(sb.toString());
-		
+
 	}
-	
+
 	private static void multiply(int N, int M, int K) {
-		
+
 		for (int n = 0; n < N; n++) {
 			for (int k = 0; k < K; k++) {
 				int answer = 0;
 				for (int m = 0; m < M; m++) {
-					answer += a_list[n][m]*b_list[m][k];
+					answer += a_list[n][m] * b_list[m][k];
 				}
-				sb.append(answer+" ");
+				sb.append(answer + " ");
 			}
 			sb.append("\n");
-			
+
 		}
-		
+
 	}
 }
