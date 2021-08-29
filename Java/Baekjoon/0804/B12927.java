debbd94a5f1563658e2245c4fa58acd13d7eca61
AYEONG <dgh03207@gmail.com>
2021-08-30 02:24:32 +0900

    Update : B10026 and B12927

--- Java/Baekjoon/0804/B12927.java
+++ Java/Baekjoon/0804/B12927.java
@@ -7,48 +7,53 @@ import java.util.Arrays;
 //배수 스위치
 public class B12927 {
 
-	static int[] switches; // switches의 정보를 저장하는 int배열
-	static int N; // switches의 사이즈를 저장하는 배열
+	static String[] switches; // switches의 정보를 저장하는 int배열
+	static int N,val; // switches의 사이즈를 저장하는 배열
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String switch_info = br.readLine();
+		String switch_info = br.readLine();			//문자열로 switch정보를 받아옴
 		int i = 1;
-		N = switch_info.length();
-		switches = new int[N + 1];
-
-		for (String sw : switch_info.split("")) {
-			if (sw.equals("Y"))
-				switches[i++] = 1;
-			else if (sw.equals("N"))
-				switches[i++] = 0;
-
-		}
+		N = switch_info.length();					//스위치의 길이를 얻음
+		switches = new String[N];					//스위치의 정보를 저장할 배열을 생성함
+		switches = switch_info.split("");
+		
 		System.out.println(work());
+		
 	}
-
+	
 	private static int work() {
 		int cnt = 0;
-		for (int j = 1; j < N + 1; j++) {
-			if (Arrays.stream(switches).sum() == 0)
-				return cnt;
-			System.out.println(j);
-			reverse(j);
-			cnt += 1;
+		for (int i = 0; i < switches.length; i++) {
+			if(switches[i].equals("Y")) {
+				turnoff(i);
+				cnt +=1; 
+			}
 		}
-		return -1;
-	}
-
-	private static void reverse(int idx) {
 		
-		for (int i = idx; i < N + 1; i += idx) {
-			if (switches[i] == 1)
-				switches[i] = 0;
-			else if (switches[i] == 0)
-				switches[i] = 1;
-			System.out.println(Arrays.toString(switches));
+		if(check(switches)) return cnt;
+		else return -1;
+	}
+	
+	private static void turnoff(int i) {
+		int start = i;
+		while(start<switches.length) {
+			if(switches[start].equals("N"))switches[start] = "Y";
+			else if(switches[start].equals("Y"))switches[start] = "N";
+			
+			start+=(i+1);
 		}
+	
+	}
+	
+	private static boolean check(String[] switches) {
+		
+		for (int i = 0; i < switches.length; i++) {
+			if(switches[i].equals("Y")) return false;
+		}
+		
+		return true;
 	}
 
 }
